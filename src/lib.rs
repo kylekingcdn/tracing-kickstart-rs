8865f449c1c17421687ea10f34671b34ca53dc11
Kyle King <git@kylek.ing>
2026-04-02 21:29:28 -0700

    Re-export OTLP dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,10 @@ mod trace;
 
 pub use trace::{ExporterBuildError, ServiceAttributeStore, TraceProviders, TracingConfig, TracingOtelConfig};
 pub use trace::{init, dump_crate_vars};
+
+pub use opentelemetry;
+pub use opentelemetry_appender_tracing;
+pub use opentelemetry_otlp;
+pub use opentelemetry_resource_detectors;
+pub use opentelemetry_sdk;
+pub use opentelemetry_semantic_conventions;
