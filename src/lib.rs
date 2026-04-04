2398a34c62052562a21deb48ae6eeed810f34778
Kyle King <git@kylek.ing>
2026-04-03 23:57:37 -0700

    Add shortform otel re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,11 @@ pub use opentelemetry_otlp;
 pub use opentelemetry_resource_detectors;
 pub use opentelemetry_sdk;
 pub use opentelemetry_semantic_conventions;
+
+// shortform exports
+pub use opentelemetry as otel;
+pub use opentelemetry_appender_tracing as otel_appender_tracing;
+pub use opentelemetry_otlp as otel_otlp;
+pub use opentelemetry_resource_detectors as otel_resource_detectors;
+pub use opentelemetry_sdk as otel_sdk;
+pub use opentelemetry_semantic_conventions as otel_semantic_conventions;
